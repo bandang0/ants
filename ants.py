a4807027aa002df9adec9e07cccc5c2d03b7c506
Gabriel Duque <naganori-san@protonmail.ch>
2016-12-27 16:39:34 +0100

    update python implementation

--- ants.py
+++ ants.py
@@ -4,11 +4,11 @@ import math
 ant_x = 0 # Ant position
 ant_y = 0
 
-start_x = 0 # Starting position
-start_y = 0
+start_x = 5 # Starting position
+start_y = 8
 
-end_x = 0 # Ending position
-end_y = 0
+end_x = 5 # Ending position
+end_y = 1
 
 path = [] # Final path
 path_length = math.inf # Final path length
@@ -16,16 +16,31 @@ path_accumulator = 0 # To know when to stop the algorithm
 # We will stop the algorithm when the same path is found 10 times in a row
 
 current = [] # Path of the current ant
-current_length = 0 # Length of the current ant
+current_length = 0 # Length of the current ant's path
 
-ph_map = [[]] # Map of pheromone quantities represented by ints to calculate the probability for the next cell, default value is 1 for all cell
-bool_map = [[]] # Map of booleans representing obstacles
+passed = [] # Passed cells for no infinite loop
 
-def pheromone_count (x,y) : # Counts the total number of pheromones surrounding the current ant's cell's
-    return(ph_map[x-1][y-1] + ph_map[x-1][y] + ph_map[x-1][y+1] + ph_map[x][y+1] + ph_map[x][y-1] + ph_map[x+1][y+1] + ph_map[x+1][y] + ph_map[x+1][y-1])
+bool_map = [[1,1,1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1,1,1]] 
+# Map of booleans repsresenting obstacles
 
-def move (x,y) :
+ph_map = [[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1]] 
+# Map of pheromones represented by ints to calculate the probability for the next cell, default value is 1 for all cells
+
+def check_list (l,a,b) :
+    a = False
+    for i in range (len (l)):
+        if l[i] == (a,b):
+            a = True
+    return a
+
+def  move (x,y) :
     
+    #global current
+    #global passed
+    global ant_x
+    global ant_y
+    global current_length
+
     a_x = x-1 # Positions of the neighboor cells
     b_x = x-1
     c_x = x-1
@@ -58,57 +73,59 @@ def move (x,y) :
 
     l = []
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[a_x][a_y] and not check_list(passed,a_x,a_y):
         for i in range (a_ph):
             l.append((a_x,a_y))
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[b_x][b_y] and not check_list(passed,b_x,b_y):
         for i in range (b_ph):
             l.append((b_x,b_y))
 
-    if not bool_map[a_x][a_y]:
-        for i in range (c-ph):
+    if not bool_map[c_x][c_y] and not check_list(passed,c_x,c_y):
+        for i in range (c_ph):
             l.append((c_x,c_y))
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[d_x][d_y] and not check_list(passed,d_x,d_y):
         for i in range (d_ph):
             l.append((d_x,d_y))
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[e_x][e_y] and not check_list(passed,e_x,e_y):
         for i in range (e_ph):
             l.append((e_x,e_y))
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[f_x][f_y] and not check_list(passed,f_x,f_y):
         for i in range (f_ph):
             l.append((f_x,f_y))
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[g_x][g_y] and not check_list(passed,g_x,g_y):
         for i in range (g_ph):
             l.append((g_x,g_y))
 
-    if not bool_map[a_x][a_y]:
+    if not bool_map[h_x][h_y] and not check_list(passed,h_x,h_y):
         for i in range (h_ph):
             l.append((h_x,h_y))
+    if l != [] :
+        rd = random.randint(0,len(l)-1)
 
-    rd = random.randint(0,len(l)-1)
+        (swap_x,swap_y) = l[rd] # Necessary swap tupple for Python to see the new position as two variables that can be independantely accessed
 
-    (swap_x,swap_y) = l[rd] # Necessary swap tupple for Python to see the new position as two variables that can be independantely accessed
+        ph_map[ant_x][ant_y] += 1 # Increment the pheromone on cell before leaving it
 
-    ph_map[ant_x][ant_y] += 1 # Increment the pheromone on cell before leaving it
-
-    ant_x = swap_x # Update ant's position
-    ant_y = swap_y
+        ant_x = swap_x # Update ant's position
+        ant_y = swap_y
     
-    current.append((ant_x,ant_y)) # Update current path after moving
-    current_length += 1
+        current.append((ant_x,ant_y)) # Update current path after moving
+        current_length += 1
+        passed.append((ant_x,ant_y))
     
 def move_call (x,y): # Function that loops untill ant is done each path
     while ant_x != end_x or ant_y != end_y:
         move (ant_x,ant_y)
+    passed = []
     
 
 
-while  path_accumulator < 10 : # Function that loops untill the final path is found
+while  path_accumulator < 1 : # Function that loops untill the final path is found
     move_call (ant_x,ant_y)
     
 for i in range (len(path)):
